f2f54ef11106207f2c79b228a463cb104717837d
longdt <dangthanhlong203@gmail.com>
2017-10-08 12:44:45 +0700

    add test class (dev only)

---
+++ JiraLite/src/main/java/jira/lite/TestData.java
@@ -0,0 +1,15 @@
+package jira.lite;
+
+import org.junit.Test;
+
+/**
+ * @author DEV-LongDT
+ *
+ */
+
+public class TestData {
+	@Test
+	public void execute() {
+		//TODO import your test code here
+	}
+}
